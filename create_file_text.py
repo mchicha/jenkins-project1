06b75c8b5e5386d5fc86390843f61e7615d23bad
Mordehai Chicha <mordehaich@gmail.com>
2021-10-18 00:21:07 +0300

    new changes

--- create_file_text.py
+++ create_file_text.py
@@ -1,4 +1,4 @@
 with open("/Users/mordehaic/Projects/final-project/jenkins-project/file.txt", "a") as f:
-    f.write("Hello my Name is Avraham ben Terah")
+    f.write("Hello my Name is Avraham ben Terah ben Haran")
     f.write("Avraham loves doing hessed")
     f.write("Avraham is one of the three fathers of the people of israel")
